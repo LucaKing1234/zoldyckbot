5a03acf78bb50d0306f9d9540312f28b32b1e677
Greg TK <33130539+GregTCLTK@users.noreply.github.com>
2018-02-07 11:18:04 +0100

    Update SECRETS.java

--- zoldyckbot/src/main/java/util/SECRETS.java
+++ zoldyckbot/src/main/java/util/SECRETS.java
@@ -2,6 +2,6 @@ package util;
 
 public class SECRETS {
 
-    public static String TOKEN = "NDA2NzQ5MzUyNTY5MjA4ODMz.DU3zxw.ouTCxB4rA-Pht5OZdMLrhJrgzqA";
+    public static String TOKEN = "TOKEN";
 
 }
